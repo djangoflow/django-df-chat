b0242e8a4eb55785514a1ab2504277ed2a804a3d
TerrenceAddison <terrenceaddison32@gmail.com>
2023-05-27 01:55:14 +0800

    Update test_message_endpoint.py

--- df_chat/tests/test_message_endpoint.py
+++ df_chat/tests/test_message_endpoint.py
@@ -27,6 +27,42 @@ class TestMessageEndpoint(APITestCase, BaseTestUtilsMixin):
         self.assertIsNone(message["parent_id"])
         self.assertFalse(message["is_reaction"])
 
+    def test_message_creation_failure_unauthenticated(self):
+        """
+        Testing failure to create a message when the user is not authenticated.
+        """
+        room = self.create_room_and_add_users()
+
+        message_endpoint = reverse("rooms-messages-list", kwargs={"room_pk": room.pk})
+        response = self.client.post(message_endpoint, {"body": "Hi"})
+        self.assertEqual(response.status_code, 401)
+
+    def test_message_creation_failure_user_not_in_room(self):
+        """
+        Testing failure to create a message when the user is not part of a room.
+        """
+        user, token = self.create_user()
+        room = self.create_room_and_add_users()
+
+        message_endpoint = reverse("rooms-messages-list", kwargs={"room_pk": room.pk})
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + token)
+        response = self.client.post(message_endpoint, {"body": "Hi"})
+        self.assertEqual(response.status_code, 201)
+
+    def test_message_creation_failure_room_not_found(self):
+        """
+        Testing failure to create a message when the room doesn't exist.
+        """
+        user, token = self.create_user()
+        non_existent_room_pk = 999
+
+        message_endpoint = reverse(
+            "rooms-messages-list", kwargs={"room_pk": non_existent_room_pk}
+        )
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + token)
+        response = self.client.post(message_endpoint, {"body": "Hi"})
+        self.assertEqual(response.status_code, 404)
+
     # TODOS: We should also implement the following tests:
     # - Fail to create a message when the user is not authenticated.
     # - Ensure that the endpoint returns a 404 error
