e6508cf38958d8a7aafe9ac8b0b80328d4dfcfdd
eugapx <111432881+eugapx@users.noreply.github.com>
2023-06-01 12:00:24 +0300

    add default pagination

--- tests/settings.py
+++ tests/settings.py
@@ -127,6 +127,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "tests.schema.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
 }
 
 SPECTACULAR_SETTINGS = {
